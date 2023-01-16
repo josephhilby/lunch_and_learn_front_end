c61cba19c3f1d89062036d7c744756b892c42b7e
Joseph Hilby <joseph.hilby@gmail.com>
2023-01-16 16:30:31 -0600

    Add routes, controllers, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#show'
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/recipes', to: 'recipes#index'
+  get '/country/:id', to: 'country#show'
+  get '/favorites', to: 'favorites#index'
 end
