d636ef4e8430dac72e2b502f732470d215f3e59f
Joseph Hilby <joseph.hilby@gmail.com>
2023-01-26 09:46:43 -0600

    Update current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id] && session[:api_key]
+      @current_user_id ||= session[:user_id]
+      @current_user_api_key ||= session[:api_key]
+    end
   end
 
   def logged_in?
