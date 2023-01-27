1bb71f869ac17751d900163e11ce70b606060487
Naomi Yocum <naomiyocum24@gmail.com>
2023-01-26 18:13:56 -0700

    Fix url for correct routing

--- app/poros/learning_resource 2.rb
+++
@@ -1,8 +0,0 @@
-class LearningResource
-  attr_reader :country, :video, :images
-  def initialize(data)
-    @country = data[:attributes][:country]
-    @video = data[:attributes][:video]
-    @images = data[:attributes][:images]
-  end
-end
